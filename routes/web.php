d900bef367181ea45a58cb6957c64820b772de21
Tameem Ahamed <98138554+tameemahamed@users.noreply.github.com>
2025-06-02 23:00:10 +0600

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Home');
 });
+
+// Method 3
+Route::inertia('/about', 'About', [
+    'user' => 'Tameem' // props
+]);
---
+++ storage/framework/views/1c11d9213cd9c1ee4267aa0bed71dc39.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <?php echo app('Illuminate\Foundation\Vite')('resources/js/app.js'); ?>
+    <?php if (!isset($__inertiaSsrDispatched)) { $__inertiaSsrDispatched = true; $__inertiaSsrResponse = app(\Inertia\Ssr\Gateway::class)->dispatch($page); }  if ($__inertiaSsrResponse) { echo $__inertiaSsrResponse->head; } ?>
+  </head>
+  <body>
+    <?php if (!isset($__inertiaSsrDispatched)) { $__inertiaSsrDispatched = true; $__inertiaSsrResponse = app(\Inertia\Ssr\Gateway::class)->dispatch($page); }  if ($__inertiaSsrResponse) { echo $__inertiaSsrResponse->body; } else { ?><div id="app" data-page="<?php echo e(json_encode($page)); ?>"></div><?php } ?>
+  </body>
+</html><?php /**PATH /home/tameem/Desktop/laravel_inertia_vue_tutorial/resources/views/app.blade.php ENDPATH**/ ?>
